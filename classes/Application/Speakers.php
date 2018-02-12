e2cb767e947b87fa5bfb8725edbde45576bce544
unknown <marinealexandre5@gmail.com>
2018-02-12 15:02:58 +0100

    porosnack add function marine

--- classes/Application/Speakers.php
+++ classes/Application/Speakers.php
@@ -72,25 +72,9 @@ class Speakers
 
         return $speaker->talks;
     }
-<<<<<<< HEAD
 
-    public function marineFunction(){
-        return "fonction de Marine";
-=======
-	
-	public function isTrue()
+    public function marineFunction()
     {
-        return true;
-    }
-
-    /**
-     * Get Host Name
-     *  Ha 0.0001x developer
-     */
-    public function getHostName() {
-        $speaker = $this->identityProvider->gethostname();
-
-        return $speaker->gethostname;
->>>>>>> b3f405769ce398fe6e5a7e3438bc8e82614c1101
+        return "fonction de Marine";
     }
 }
